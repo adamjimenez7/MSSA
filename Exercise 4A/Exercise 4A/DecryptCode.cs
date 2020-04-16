9c0f46e9b5eac0bc83a757b178d1eb9a3e91a6e0
atomjimenez <59895035+atomjimenez@users.noreply.github.com>
2020-04-16 18:12:39 -0400

    Uploading cipher encryption/decryption program

---
+++ Exercise 4A/Exercise 4A/DecryptCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_4A
+{
+    class DecryptCode
+    {
+        public string DecryptMessage(string stringToDecrypt, string DecryptionCode)
+        {
+            string decryptedString = ""; //Empty variable to hold the new string since strings are immutable
+            int decryptionSub; //Variable to determine ASCII value after decryption operations
+            int count = 0; //temp variable to keep track of place in encryption key
+
+            //**********Use when you want to repeat decryption code, e.g. DC = "CAT", will decrypt using CATCATCATCAT**********
+            //for (int i = 0; i < stringToDecrypt.Length; i++)
+            //{
+            //    int cypherLoop = count % DecryptionCode.Length; //Variable to determine index of decryption code when decryption will use same code repeatedly
+            //    decryptionSub = (stringToDecrypt[i] - (DecryptionCode[cypherLoop] - 64)) > 64 ? stringToDecrypt[i] - (DecryptionCode[cypherLoop] - 64) : (stringToDecrypt[i] - (DecryptionCode[cypherLoop] - 64)) + 26; //Ensure encrypted letters still have ASCII values of capital letters
+            //    char character = (char)decryptionSub;
+            //    decryptedString += (character.ToString());
+            //    count++;
+            //}
+
+            //**********Use when you want to use entire Decryption key, then the message itself to decrypt, e.g. DC = "CAT", will decrypt using CATABCD....N where ABCD...N = first N letters of string to decrypt**********
+            for (int i = 0; i < stringToDecrypt.Length; i++)
+            {
+                if (count < DecryptionCode.Length) //If count is shorter than decryption code length (we have not used every letter in decryption string)
+                {
+                    decryptionSub = (stringToDecrypt[i] - (DecryptionCode[count] - 64)) > 64 ? stringToDecrypt[i] - (DecryptionCode[count] - 64) : (stringToDecrypt[i] - (DecryptionCode[count] - 64)) + 26;
+                    char character = (char)decryptionSub;
+                    decryptedString += (character.ToString());
+                    count++;
+                }
+
+                else //After we exhaust decryption string, start at the first letter of string to decrypt
+                {
+                    decryptionSub = (stringToDecrypt[i] - (decryptedString[count - DecryptionCode.Length] - 64)) > 64 ? stringToDecrypt[i] - (decryptedString[count - DecryptionCode.Length] - 64) : (stringToDecrypt[i] - (decryptedString[count - DecryptionCode.Length] - 64)) + 26;
+                    char character = (char)decryptionSub;
+                    decryptedString += (character.ToString());
+                    count++;
+                }
+            }
+            return decryptedString;
+        }
+    }
+}
---
+++ Exercise 4A/Exercise 4A/EncryptCode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Exercise_4A
+{
+    class EncryptCode
+    {       
+        public string EncryptMessage(string stringToEncrypt, string encryptionCode)
+        {
+            string encryptedString = ""; //Empty variable to hold the new string since strings are immutable
+            int encryptionAdd; //Variable to determine ASCII value after decryption operations
+            int count = 0; //temp variable to keep track of place in encryption key
+
+            //**********Use when you want to repeat encryption code, e.g. EC = "CAT", will encrypt using CATCATCATCAT**********
+            /*
+            for (int i = 0; i < stringToEncrypt.Length; i++)
+            {
+                int cypherLoop = count % encryptionCode.Length; //Variable to determine index of encryption code when encryption will use same code repeatedly
+                encryptionAdd = (stringToEncrypt[i] + (encryptionCode[cypherLoop] - 64)) < 91 ? stringToEncrypt[i] + (encryptionCode[cypherLoop] - 64) : (stringToEncrypt[i] + (encryptionCode[cypherLoop] - 64)) % 91 + 65; //Ensure encrypted letters still have ASCII values of capital letters
+                char character = (char) encryptionAdd;
+                encryptedString += (character.ToString()); 
+                count++;
+            }
+            */
+
+            //**********Use when you want to use entire encryption key, then the message itself to encrypt, e.g. EC = "CAT", will encrypt using CATABCD....N where ABCD...N = first N letters of string to encrypt**********
+            for (int i = 0; i < stringToEncrypt.Length; i++)
+            {
+                if (count < encryptionCode.Length) //If count is shorter than encryption code length (we have not used every letter in encryption string)
+                {
+                    encryptionAdd = (stringToEncrypt[i] + (encryptionCode[count] - 64)) < 91 ? stringToEncrypt[i] + (encryptionCode[count] - 64) : (stringToEncrypt[i] + (encryptionCode[count] - 64)) % 91 + 65;
+                    char character = (char)encryptionAdd;
+                    encryptedString += (character.ToString());
+                    count++;
+                }
+                else //After we exhaust encryption string, start at the first letter of the string to encrypt
+                {
+                    encryptionAdd = (stringToEncrypt[i] + (stringToEncrypt[count - encryptionCode.Length] - 64)) < 91 ? stringToEncrypt[i] + (stringToEncrypt[count - encryptionCode.Length] - 64) : (stringToEncrypt[i] + (stringToEncrypt[count - encryptionCode.Length] - 64)) % 91 + 65;
+                    char character = (char)encryptionAdd;
+                    encryptedString += (character.ToString());
+                    count++;
+                }
+            }
+
+            return encryptedString;
+        }
+    }
+}
